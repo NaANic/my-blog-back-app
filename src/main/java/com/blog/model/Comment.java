1dadca4a85949f803d54a034edc3bb2e15805c8b
Alexey Nagatkin <a.nagatkin@hh.ru>
2026-02-12 23:08:49 +0300

    feat: add domain models
    - Create Post entity with tags, likes, comments count
    - Create Comment entity with cascade delete
    - Add Spring Data JDBC annotations

--- src/main/java/com/blog/model/Comment.java
+++ src/main/java/com/blog/model/Comment.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -11,15 +12,19 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table("comments")
+@Table("COMMENTS")
 public class Comment {
 
   @Id
+  @Column("ID")
   private Long id;
 
+  @Column("POST_ID")
   private Long postId;
 
+  @Column("TEXT")
   private String text;
 
+  @Column("CREATED_AT")
   private LocalDateTime createdAt;
 }
